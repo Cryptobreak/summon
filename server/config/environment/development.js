00a58da56ffe2901c3e343e73215ebf72cc4046c
peppertech <peppertech@gmail.com>
2014-07-16 17:28:37 -0700

    new framework2

---
+++ server/config/environment/development.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    //uri: 'mongodb://localhost/testproject-dev'
+	uri: 'mongodb://summondevs:sleepingforest12@ds053139.mongolab.com:53139/summon'
+  }
+};
